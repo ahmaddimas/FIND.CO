58fc3ad862cf95f31eb8e01dd8957de7072d06e7
ahmaddimas <ahmaddimas0@gmail.com>
2017-12-07 12:53:26 +0700

    add template

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -9,7 +9,7 @@ class Home extends CI_Controller {
 
 	public function index() {
 		$data = [
-			'main_view'	=> 'main'
+			'main_view'	=> 'dashboard'
 		];
 		$this->load->view('layout', $data);
 	}
